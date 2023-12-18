6ae7313bd3fa474f6f62e66e89ad65853c08638f
silviosalza <silviofilipposalza@gmail.com>
2023-12-18 12:29:19 +0100

    seeder and reamde added

---
+++ !!README!!.txt
@@ -0,0 +1,18 @@
+Istruzioni per popolare il database mySql tramite seeder:
+
+- Accendere MAMP
+- nel terminale:
+
+1- cd "{il vostro percorso dove avete clonato la repo}"\react-il-mio-fotoalbum\BACKEND"
+2- npm start
+3- cd "{il vostro percorso dove avete clonato la repo}\react-il-mio-fotoalbum\BACKEND\prisma\seeders"
+4- node seeder.js              
+
+
+Dopo aver popolato il db è possibile lanciare l'app
+
+in un nuovo terminale: 
+
+1- cd "{il vostro percorso dove avete clonato la repo}\react-il-mio-fotoalbum\FRONTEND"
+2- npm run dev
+
--- BACKEND/prisma/seeders/seeder.js
+++ BACKEND/prisma/seeders/seeder.js
@@ -6,23 +6,100 @@ const prisma = new PrismaClient();
 
 async function seedDatabase() {
   try {
-    // Popola il database con dati specifici e immagini dalla cartella uploads
+    // Popolo il database con dati specifici e immagini dalla cartella uploads
     const postsData = [
       {
         title: 'Titolo 1',
         content: 'Contenuto del post 1',
         published: true,
-        image: 'image-1702810325957.jpg', // Assicurati che l'immagine esista nella cartella uploads
-        tagIds: [1, 2], // Array di ID delle tags da associare
+        image: 'image-1702810325957.jpg', 
+        tagIds: [1, 2],
       },
       {
         title: 'Titolo 2',
         content: 'Contenuto del post 2',
         published: true,
-        image: 'image-1702810325957.jpg', // Assicurati che l'immagine esista nella cartella uploads
-        tagIds: [2, 3], // Array di ID delle tags da associare
+        image: 'image-1702810579326.jpg', 
+        tagIds: [2, 3], 
       },
-      // Aggiungi altri dati secondo necessità
+      {
+        title: 'Titolo 3',
+        content: 'Contenuto del post 3',
+        published: true,
+        image: 'image-1702810602680.jpg', 
+        tagIds: [2], 
+      },
+      {
+        title: 'Titolo 4',
+        content: 'Contenuto del post 4',
+        published: true,
+        image: 'image-1702810827409.jpg', 
+        tagIds: [1], 
+      },
+      {
+        title: 'Titolo 5',
+        content: 'Contenuto del post 5',
+        published: true,
+        image: 'image-1702827832792.jpg', 
+        tagIds: [5], 
+      },
+      {
+        title: 'Titolo 6',
+        content: 'Contenuto del post 6',
+        published: true,
+        image: 'image-1702827711541.jpg', 
+        tagIds: [3], 
+      },
+      {
+        title: 'Titolo 7',
+        content: 'Contenuto del post 7',
+        published: true,
+        image: 'image-1702827633009.jpg', 
+        tagIds: [1], 
+      },
+      {
+        title: 'Titolo 8',
+        content: 'Contenuto del post 8',
+        published: true,
+        image: 'image-1702827737099.jpg', 
+        tagIds: [2, 3], 
+      },
+      {
+        title: 'Titolo 9',
+        content: 'Contenuto del post 9',
+        published: true,
+        image: 'image-1702827605584.jpg', 
+        tagIds: [3], 
+      },
+      {
+        title: 'Titolo 10',
+        content: 'Contenuto del post 10',
+        published: true,
+        image: 'image-1702811055989.jpg', 
+        tagIds: [1], 
+      },
+      {
+        title: 'Titolo 11',
+        content: 'Contenuto del post 11',
+        published: true,
+        image: 'image-1702811025736.jpg', 
+        tagIds: [5], 
+      },
+      {
+        title: 'Titolo 12',
+        content: 'Contenuto del post 12',
+        published: true,
+        image: 'image-1702827753232.jpg', 
+        tagIds: [1], 
+      },
+      {
+        title: 'Titolo 13',
+        content: 'Contenuto del post 13',
+        published: true,
+        image: 'image-1702827659835.jpg', 
+        tagIds: [4], 
+      },
+      
     ];
 
     for (const postData of postsData) {
@@ -34,7 +111,7 @@ async function seedDatabase() {
           title: postData.title,
           content: postData.content,
           published: postData.published,
-          image: postData.image, // Passa direttamente il nome del file
+          image: postData.image, 
           tags: {
             connect: postData.tagIds.map(tagId => ({ id: tagId })),
           },
@@ -52,5 +129,5 @@ async function seedDatabase() {
   }
 }
 
-// Esegui lo script di seeding
+
 seedDatabase();
